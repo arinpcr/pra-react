324ee6b5acbfed720805367e8d87cb6f60ba89b5
arinpcr <arini24si@mahasiswa.pcr.ac.id>
2026-04-14 23:02:39 +0700

    feat: add Sedap Dashboard and 3 Improvisations PFL Pertemuan 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import CatManagement from './tugas/pertemuan4/CatManagement';
+import Sidebar from './tugas/pertemuan5/layout/Sidebar';
+import Header from './tugas/pertemuan5/layout/Header';
+import PageHeader from './tugas/pertemuan5/components/PageHeader';
+import Dashboard from './tugas/pertemuan5/pages/Dashboard';
 
 function App() {
   return (
-    <CatManagement />
+    <div className="min-h-screen bg-latar font-poppins text-teks">
+      <div className="flex min-h-screen flex-col lg:flex-row">
+        <Sidebar />
+        <main className="flex-1 p-4 md:p-6 xl:p-8">
+          <Header />
+          <div className="mt-6 space-y-6">
+            <PageHeader />
+            <Dashboard />
+          </div>
+        </main>
+      </div>
+    </div>
   );
 }
 
